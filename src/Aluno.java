2692a68acc80c75895b3ddeba55a72f60ead4677
Rafael Gonzaga <ralozaga@bol.com.br>
2021-09-03 12:30:34 -0300

    First commit

---
+++ src/Aluno.java
@@ -0,0 +1,29 @@
+public class Aluno {
+    private String nome;
+    private String email;
+    private String numeroMatricula;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(String numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+}
---
+++ src/Professor.java
@@ -0,0 +1,29 @@
+public class Professor {
+    private String nome;
+    private String email;
+    private String numeroEmpregado;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroEmpregado() {
+        return numeroEmpregado;
+    }
+
+    public void setNumeroEmpregado(String numeroEmpregado) {
+        this.numeroEmpregado = numeroEmpregado;
+    }
+}
